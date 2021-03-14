ff8d07f747cdcd3a739d88df481553a85644fa99
Aram <aram1993abovyan@gmail.com>
2021-03-15 01:20:32 +0400

    added commands to manipulate data

--- Comands.sql
+++ Comands.sql
@@ -1,6 +1,9 @@
 -- get average result per course
 
-
+select course_id, AVG(result) AS "Average Result"
+from "public"."results"
+GROUP BY course_id
+ORDER BY course_id
 
 -- get students that have maximum result in each course
 
@@ -11,4 +14,23 @@ Join
     Select  course_id, Max(result) As "result"
     From "public"."results"
     Group By course_id
-) D On results.result = D.result And results.course_id = D.course_id ORDER BY results.course_id
+) D On results.result = D.result And results.course_id = D.course_id
+ORDER BY results.course_id
+
+-- get first 10 students per course that get highest results
+
+SELECT
+  *
+FROM
+  "public"."results" a
+WHERE (
+  SELECT
+    COUNT(5)
+  FROM
+    "public"."results"
+  WHERE
+    course_id = a.course_id
+  AND
+    result >= a.result
+) <= 10
+ORDER BY course_id
